4d942ef2ae5cef77a8aa088618d443ede092e579
CTO <cto@kayadami.com>
2015-01-06 23:58:42 +0900

    first commit

---
+++ happinuss_ios/happinuss_ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  happinuss_ios
+//
+//  Created by Imac on 2015. 1. 6..
+//  Copyright (c) 2015년 kayadami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ happinuss_ios/happinuss_ios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  happinuss_ios
+//
+//  Created by Imac on 2015. 1. 6..
+//  Copyright (c) 2015년 kayadami. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
